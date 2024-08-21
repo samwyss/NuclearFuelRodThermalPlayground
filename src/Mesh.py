7b5f6428c67ef3332550f30f4ff0ac1714fa33a7
Sam Wyss <sjwyss@outlook.com>
2024-08-21 08:04:48 -0400

    added docstrings

--- src/Mesh.py
+++ src/Mesh.py
@@ -1,6 +1,10 @@
 class Mesh:
+    """represents and provides a high level interface for the simulation mesh"""
+
     def __init__(self):
+        """Mesh constructor"""
         pass
 
     def __repr__(self):
+        """changes how Mesh is represented, useful for debugging"""
         pass
--- src/Solver.py
+++ src/Solver.py
@@ -1,6 +1,10 @@
 class Solver:
+    """provides a facade for operating the thermal solver"""
+
     def __init__(self):
+        """Solver constructor"""
         pass
 
     def __repr__(self):
+        """changes how Solver is represented, useful for debugging"""
         pass
