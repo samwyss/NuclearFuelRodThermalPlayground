709ae4335c9c03c567bde51453f933d45f351008
Sam Wyss <sjwyss@outlook.com>
2024-09-26 15:35:12 -0400

    populated main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,37 @@
-from Solver import Solver
+from src.Config import Config
+from src.Model import Model
+
 
 def main():
     """main driver function for ThermalPlayground"""
-    solver = Solver()
+
+    # todo add gui logic here
+
+    # when "run" button or something is clicked run this code
+    fuel_thickness: float = 0.0
+    cladding_thickness: float = 0.0
+    bulk_material_temp: float = 0.0
+    coolant_temp: float = 0.0
+    core_heat_generation = 0.0
+    num_saved_time_steps = 0
+    config = Config(
+        fuel_thickness,
+        cladding_thickness,
+        bulk_material_temp,
+        coolant_temp,
+        core_heat_generation,
+        num_saved_time_steps,
+    )
+
+    # construct model
+    model: Model = Model(config)
+
+    # run model
+    model.run()
+
+    # todo post processing
+
 
 # runs main
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
