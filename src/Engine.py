1be85a7f8028ded497a1a6817c78483b49cc4ec2
Sam Wyss <sjwyss@outlook.com>
2024-09-26 15:51:44 -0400

    added todo statements regarding work that needs done

--- src/Engine.py
+++ src/Engine.py
@@ -8,12 +8,20 @@ class Engine:
 
     def __init__(self, config: Config):
         """Solver constructor"""
+
+        # todo set up material properties
+
+        # todo set up mesh stuff
+
         pass
 
     def __repr__(self) -> dict[str, Any]:
         """changes how Solver is represented, useful for debugging"""
         return self.__dict__
 
-    def update(self):
+    def update(self) -> None:
         """updates the model to the next timestep"""
-        pass
+
+        # todo update material properties
+
+        # todo update temperature
