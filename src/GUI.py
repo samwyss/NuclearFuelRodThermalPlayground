ae96252b6919d528bb059ac29d84e50d4bb5e0fb
akom1220 <allisonkomrska@gmail.com>
2024-10-16 14:57:57 -0400

    more buttons

--- src/GUI.py
+++ src/GUI.py
@@ -24,20 +24,39 @@ entry_6 = Entry(root)
 entry_7 = Entry(root)
 
 label_1.grid(row = 4, sticky = E)
-label_2.grid(row = 5, sticky = E)
-label_3.grid(row = 6, sticky = E)
-label_4.grid(row = 7, sticky = E)
-label_5.grid(row = 8, sticky = E)
-label_6.grid(row = 9, sticky = E)
-label_7.grid(row = 10, sticky = E)
+label_2.grid(row = 6, sticky = E)
+label_3.grid(row = 8, sticky = E)
+label_4.grid(row = 10, sticky = E)
+label_5.grid(row = 12, sticky = E)
+label_6.grid(row = 14, sticky = E)
+label_7.grid(row = 16, sticky = E)
 
-entry_1.grid(row = 4, column = 1)
-entry_2.grid(row = 5, column = 1)
-entry_3.grid(row = 6, column = 1)
-entry_4.grid(row = 7, column = 1)
-entry_5.grid(row = 8, column = 1)
-entry_6.grid(row = 9, column = 1)
-entry_7.grid(row = 10,column = 1)
+entry_1.grid(row = 4, column = 3)
+entry_2.grid(row = 6, column = 3)
+entry_3.grid(row = 8, column = 3)
+entry_4.grid(row = 10, column = 3)
+entry_5.grid(row = 12, column = 3)
+entry_6.grid(row = 14, column = 3)
+entry_7.grid(row = 16,column = 3)
+def exitwindow():
+    print("\nWindow is being closed.")
+    return
+
+def startsim():
+    print("\nSimulation is running.")
+    return
+
+def analyzeresults():
+    print("\nResults are being analyzed")
+    return
+
+button_1 = Button(root, text = "Exit", command = exitwindow)
+button_2 = Button(root, text = "Run", command = startsim)
+button_3 = Button(root, text = "Analyze Results", command = analyzeresults)
+button_1.grid(row = 20, column = 1)
+button_2.grid(row = 20, column = 2)
+button_3.grid(row = 20, column = 3)
+
 
 
 root.mainloop()
