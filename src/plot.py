cb15dcf8aff22bfffa3d3a6774d6fd704e9a9f9f
samwyss_beeplab2 <wysss@purdue.edu>
2024-10-23 12:08:00 -0400

    made plot prettier

--- src/plot.py
+++ src/plot.py
@@ -4,8 +4,19 @@ import numpy as np
 temps = np.loadtxt("./out/temperature.csv", delimiter=",")
 pos = np.loadtxt("./out/position.csv", delimiter=",")
 time = np.loadtxt("./out/time.csv", delimiter=",")
+max_temp = np.max(temps)
+min_temp = np.min(temps)
+min_pos = np.min(pos)
+max_pos = np.max(pos)
+
 
 for i in range(temps.shape[0]):
-    plt.plot(pos, temps[i, :])
+    plt.plot(pos, temps[i, :], label=f"{time[i]:.3f}")
+
+plt.xlim((min_pos, max_pos))
+plt.ylim((min_temp, max_temp))
+plt.xlabel("Radial Position [m]")
+plt.ylabel("Temperature [K]")
+plt.legend(bbox_to_anchor=(1, 1), title="Time [s]")
 
 plt.show()
