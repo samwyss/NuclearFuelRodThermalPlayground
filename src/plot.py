4eac43a5b87c7b286cda813d6c6ba74f99c818de
samwyss_beeplab2 <wysss@purdue.edu>
2024-10-23 11:43:02 -0400

    files saved to outdir

--- src/plot.py
+++ src/plot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-temps = np.loadtxt("temperature.csv", delimiter=",")
-pos = np.loadtxt("position.csv", delimiter=",")
-time = np.loadtxt("time.csv", delimiter=",")
+temps = np.loadtxt("./out/temperature.csv", delimiter=",")
+pos = np.loadtxt("./out/position.csv", delimiter=",")
+time = np.loadtxt("./out/time.csv", delimiter=",")
 
 for i in range(temps.shape[0]):
     plt.plot(pos, temps[i, :])
