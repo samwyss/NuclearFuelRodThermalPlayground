94609f43651a32b48270d4b9c8e2ad2f0e1cae22
samwyss_beeplab2 <wysss@purdue.edu>
2024-11-05 09:02:54 -0500

    changed figure size

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.ma.core import shape
 
 temps = np.loadtxt("./out/temperature.csv", delimiter=",")
 pos = np.loadtxt("./out/position.csv", delimiter=",")
@@ -11,14 +12,18 @@ max_pos = np.max(pos)
 
 plt.rcParams["figure.dpi"] = 300
 
+fig, ax = plt.subplots(figsize=(8,6), dpi=300)
+
 r = np.linspace(0, 0.1, 1000)
-plt.plot(r, (1e6*(0.1**2 - r**2)) / (4 * 10), label="Theoretical Steady State")
-plt.plot(pos, temps[-1, :], label=f"CN Steady State")
+plt.plot(r, (1e6*(0.1**2 - r**2)) / (4 * 10), "k", label="Theory SS")
+
+for i in range(temps.shape[0]):
+    plt.plot(pos, temps[i, :], ".", label=f"t={time[i]/3600:.1f} [h]")
 
 plt.xlim((0, 0.1))
 plt.ylim((0, 250))
 plt.xlabel("Radial Position [m]")
 plt.ylabel("Temperature [K]")
-plt.legend()
+plt.legend(bbox_to_anchor=(1, 1))
 
 plt.show()
